44a813410d54dfeb8bc65e54bba16ac5c45ff77c
Ressy911 <markpiddubnyi@gmail.com>
2021-02-01 22:02:49 +0200

    updated conftest webdriver params

--- conftest.py
+++ conftest.py
@@ -1,2 +1,14 @@
+import pytest
+from selenium import webdriver
 
+@pytest.fixture(params=["Chrome", "Firefox"], scope='class')
+def init_driver(request):
+    if request.param == "Chrome":
+        driver = webdriver.Chrome()
+    elif request.param == "Firefox":
+        driver = webdriver.Firefox()
+
+    request.cls.driver = driver
+    yield
+    driver.close()
 
